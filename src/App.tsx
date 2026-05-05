53cd257ac12124f18c512dfc65f3ea46b96dfd9d
Dhiraj Shahi <151495872+hackerizzzboi@users.noreply.github.com>
2026-05-05 15:26:11 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Admin from './pages/Admin';
+import { AdminGuard } from './components/AdminGuard';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
